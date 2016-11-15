147adccb39506a011424ad7e6ec14ba1c0de7401
tornadoGH <kooka@eink.com>
2016-11-15 14:52:24 +0900

    Listing2_8 ウィンドウにプログレスインジケータを表示(非推奨API かつ 表示されない）

---
+++ listing2_8/src/main/java/com/example/listing2_8/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.listing2_8;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
+        setContentView(R.layout.activity_main);
+
+        setProgressBarIndeterminateVisibility( true );
+    }
+}
